ed2e2517f04bb8dc72a8496221bfac50d8a59ab6
matheus.tambosi <tambosi.matheus@gmail.com>
2024-05-01 21:12:17 -0300

    solução desafio 3 POO

--- trilha-net-poo-desafio-main/trilha-net-poo-desafio-main/Program.cs
+++ trilha-net-poo-desafio-main/trilha-net-poo-desafio-main/Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone(
+        numero: "8479",
+        modelo: "13 Pro",
+        IMEI: "123123123",
+        memoria: 12);
+
+Smartphone nokia = new Nokia(
+        numero: "3567",
+        modelo: "11",
+        IMEI: "123444444",
+        memoria: 8);
+
+Console.WriteLine("Nokia:");
+nokia.InstalarAplicativo("Music Dash");
+nokia.Ligar();
+
+Console.WriteLine("Iphone:");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Circle dash");
